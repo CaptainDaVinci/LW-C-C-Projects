831ce32494b9e33ab1898a66e545a5a5ca1658a4
Nathan Masi <nathanmasi8@hotmail.com>
2017-02-04 19:41:38 +0000

    Delete stuff.h

--- stuff.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-
-bool strequ(char text1[], char text2[]);
-void stcl();
-int rannum(int min_num, int max_num);
