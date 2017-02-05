657fa47d07c0758e28a6b5b2d534ea73765649dc
CaptainDaVinci <yashkothari199767@gmail.com>
2017-02-05 16:32:30 +0530

    Better formatting

--- typespeedgame/stuff.c
+++ typespeedgame/stuff.c
@@ -7,50 +7,39 @@
 #include <unistd.h>
 #include <time.h>
 
+void stcl();
 bool strequ(char text1[], char text2[]){
-	/*char c1ch;
-	char c2ch;
-	int a = 0;
-	while(c1ch != '\0'){
-		c1ch = text1[a];
-		c2ch = text2[a];
-		if(c1ch != c2ch){
-			return false;
-		}
-		a++;
-	}
-	return true;*/
 	if(strcmp(text1, text2) == 0){
 		return true;
 	}else{
 		return false;
 	}
 }
-void stcl();
 void stcl(){
 	system("clear"); // change clear to cls if your on windows
 }
 
 int rannum(int min_num, int max_num){
-	long long int result=0,low_num=0,hi_num=0;
-	if(min_num<max_num){
-		low_num=min_num;
-		hi_num=max_num+1; // this is done to include max_num in output.
+	long long int result = 0, low_num = 0, hi_num = 0;
+
+	if(min_num < max_num){
+		low_num = min_num;
+		hi_num = max_num+1; // this is done to include max_num in output.
 	}else{
-		low_num=max_num+1;// this is done to include max_num in output.
-		hi_num=min_num;
+		low_num = max_num+1;// this is done to include max_num in output.
+		hi_num = min_num;
 	}
-	
+
 	result = ((rand())%(hi_num-low_num))+low_num;
 	return result;
 }
 
 void anicel(char text[50]){
-	
+
 	//text position
 	int tx = 0;
 	int ty = 0;
-	
+
 	//loop for 40 times
 	int a;
 	for(a = 0; a < 20; a++){
@@ -73,7 +62,7 @@ void anicel(char text[50]){
 		printf("%s\n", text);
 		printf("=========================================\n");
 		usleep(100000);
-		
+
 	}
 	stcl();
 }
