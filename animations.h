452756e35e46cd5b9fcffa4262cab6f336a2fc67
Nathan Masi <nathanmasi8@hotmail.com>
2017-02-05 20:05:32 +0000

    Animations project
    All part of the animations project of showing cool animations

---
+++ animations.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+int los(char *s);
+void stcl();
+
+void ttltbr(char text[40]);
+void bounc(char text[]);
+void bubl(char text[]);
+void stripe(char text[], int times);
---
+++ build.sh
@@ -0,0 +1,3 @@
+ echo Build Started
+ gcc main.c animations.h animations.c -o text_anims
+ echo Build Ended
