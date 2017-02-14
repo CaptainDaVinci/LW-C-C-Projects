418733e22ab0a47328e348a497a8ffed088fee4d
CaptainDaVinci <yashkothari199767@gmail.com>
2017-02-14 23:07:40 +0530

    Added a structure and removed stuff.h file as it is no longer needed

--- typespeedgame/play.h
+++ typespeedgame/play.h
@@ -1,12 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-int score;
-void play();
+#ifndef __TYPING_H__
+#define __TYPING_H__
 
-bool hsbu;
-void settings();
-int sentences;
-int stohs;
+#include <stdbool.h>
+
+typedef struct
+{
+	bool colour, animate;
+	int score, streak, cor, td, i;
+
+}Game;
+
+Game *game;
+
+void play(Game *);
+void settings(Game *);
+void stcl(void);                            // clear screen
+int rannum(int min_num, int max_num);       // generate a random number
+void anicel(int streak);                    // animation for celebration :)
+
+#endif
--- typespeedgame/stuff.c
+++ typespeedgame/stuff.c
@@ -1,68 +1,48 @@
-#include "stuff.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
-#include <stdbool.h>
 #include <unistd.h>
 #include <time.h>
 
-void stcl();
-bool strequ(char text1[], char text2[]){
-	if(strcmp(text1, text2) == 0){
-		return true;
-	}else{
-		return false;
-	}
-}
-void stcl(){
-	system("clear"); // change clear to cls if your on windows
+void stcl()
+{
+	system("clear");
 }
 
-int rannum(int min_num, int max_num){
-	long long int result = 0, low_num = 0, hi_num = 0;
-
-	if(min_num < max_num){
-		low_num = min_num;
-		hi_num = max_num+1; // this is done to include max_num in output.
-	}else{
-		low_num = max_num+1;// this is done to include max_num in output.
-		hi_num = min_num;
-	}
-
-	result = ((rand())%(hi_num-low_num))+low_num;
-	return result;
-}
-
-void anicel(char text[50]){
+void anicel(int streak)
+{
 
 	//text position
-	int tx = 0;
-	int ty = 0;
+	int tx, ty, a, b, e;
+	tx = ty = a = b = e = 0;
 
-	//loop for 40 times
-	int a;
-	for(a = 0; a < 20; a++){
+	//loop for 20 times
+	for(a = 0; a < 20; a++)
+	{
 		//positioning
 		ty = a;
 		tx++;
+		e = b = 0;
+
 		//printing
 		stcl();
 		printf("=========================================\n");
-		int e = 0;
-		while(e < ty){
+
+		while(e < ty)
+		{
 			printf(" \n");
 			e++;
 		}
-		int b = 0;
-		while(b < tx){
+		while(b < tx)
+		{
 			printf(" ");
 			b++;
 		}
-		printf("%s\n", text);
+		printf("You reached streak %d!\n", streak);
 		printf("=========================================\n");
 		usleep(100000);
-
 	}
+
 	stcl();
 }
